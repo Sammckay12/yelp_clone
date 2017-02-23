06d5f1d1b108d587e0f918b5bba9357aa46cb89f
Sam Mckay <sammckay12@gmail.com>
2017-02-23 16:54:58 +0000

    associations for reviews with user

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
-  has_many :reviews, dependent: :destroy
+  has_many :reviews,
+      -> { extending WithUserAssociationExtension },
+      dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
 
 
@@ -9,5 +11,12 @@ class Restaurant < ApplicationRecord
     review.user = current_user
     review
   end
+
+  has_many :reviews do
+  def build_with_user(attributes = {}, user)
+    attributes[:user] ||= user
+    build(attributes)
+  end
+end
 
 end
